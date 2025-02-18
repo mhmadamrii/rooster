6f1b5eaca1477076f21843f31499f70efce681d3
mhmadamrii <interceptorghost4@gmail.com>
2025-02-18 20:54:20 +0700

    rs: rushhhhhh

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,46 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Post {
+    id: i32,
+    title: String,
+    content: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("Hello, Actix!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+#[get("/posts")]
+async fn get_posts() -> impl Responder {
+    let posts = vec![
+        Post {
+            id: 1,
+            title: String::from("First Post"),
+            content: String::from("This is my first post content"),
+        },
+        Post {
+            id: 2,
+            title: String::from("Second Post"),
+            content: String::from("This is my second post content"),
+        },
+    ];
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().json(posts)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Server running at http://127.0.0.1:8080");
+    
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(get_posts)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
