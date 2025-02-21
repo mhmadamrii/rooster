1cf0cd9d007b9728f849dbd5c41c8e9be706723e
mhmadamrii <interceptorghost4@gmail.com>
2025-02-21 16:06:11 +0700

    fdlka

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,45 @@
-fn main() {
-    greeting("Bob");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Info {
+    name: String,
+    age: Option<u32>,
 }
 
-fn greeting(name: &str) {
-    println!("Hello {}", name);
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[get("/query")]
+async fn with_params(info: web::Query<Info>) -> impl Responder {
+    let response = format!(
+        "Hello {}! {}",
+        info.name,
+        info.age.map_or(String::from(""), |age| format!("You are {} years old.", age))
+    );
+    HttpResponse::Ok().body(response)
+}
+
+async fn manual_something() -> impl Responder {
+    HttpResponse::Ok().body("Hey there, this is a manual something!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(with_params)
+            .route("/hey", web::get().to(manual_hello))
+            .route("/something", web::get().to(manual_something))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
